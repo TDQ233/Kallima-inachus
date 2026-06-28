d9998e9b6cd849cdf7129488985dd311ba4db7c7
TDQ233 <78018442+TDQ233@users.noreply.github.com>
2026-06-28 20:00:37 +0800

    Delete 2026 directory

--- 2026/HiCExplorer.sh
+++
@@ -1,8 +0,0 @@
-hicConvertFormat -m Kin_P_chr26_5k.matrix -bf Kin_P_chr26_5k.bed --inputFormat hicpro --outputFormat h5 -o Kin_P_chr26_5k -r 5000
-
-hicCorrectMatrix correct --matrix Kin_P_chr26_5k.h5 --correctionMethod KR -o Kin_P_chr26_5k_KR.h5
-
-hicFindTADs --matrix Kin_P_chr26_5k_KR.h5 --outPrefix Kin_P_chr26_5k_KR --correctForMultipleTesting fdr
-
-hicPlotTADs --tracks test.ini -o test.pdf --region chr26:3800000-4600000 --dpi 600
-
--- 2026/RAxML.sh
+++
@@ -1,4 +0,0 @@
-raxmlHPC -f a -m GTRGAMMA -p 12345 -x 12345 -# 100 -T 20 -s nucleotide.phy -n nucleotide_tree
-
-raxmlHPC -f a -m PROTGAMMALG -p 12345 -x 12345 -# 100 -T 20 -s protein.phy -n protein_tree
-
